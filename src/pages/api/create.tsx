a4f7f349b3682a2e39416598bc9797cf7297981f
Vidit Khandelwal <viditk17@gmail.com>
2023-07-16 01:27:22 -0400

    added catch statement to ChatCompletions Promise resolution

--- src/pages/api/create.tsx
+++ src/pages/api/create.tsx
@@ -2,6 +2,7 @@ import { openai } from "@/openai.config";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ChatCompletionRequestMessage } from "openai";
 import * as _ from "lodash";
+import * as fs from "graceful-fs";
 
 export default async function handler(
   req: NextApiRequest,
@@ -27,44 +28,50 @@ export default async function handler(
         } I want you to format your answer in three lines this format: \nSuggested name of the business\nDescription of the business\nA JavaScript array of possible domain names`,
       },
     ];
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: prompt,
-    });
-    const response = completion.data.choices[0].message?.content?.split("\n");
-    if (response != undefined) {
-      // console.log(response[2]?.substring(response[2].indexOf(":") + 1));
-      const businessName = response[0]
-        ?.substring(response[0].indexOf(":") + 1)
-        .replace(/"/g, "");
-      const businessDescription = response[1]?.substring(
-        response[1].indexOf(":") + 1
-      );
-      let businessDomainsString = response[2]?.substring(
-        response[2].indexOf(":") + 1
-      );
-      businessDomainsString.replace(/[\[\]]/g, "");
-      businessDomainsString = businessDomainsString
-        .trim()
-        .slice(1, -1)
-        .replace(/'/g, "");
-      const businessDomains = businessDomainsString.split(",");
+    await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: prompt,
+      })
+      .then((completion) => {
+        const response =
+          completion.data.choices[0].message?.content?.split("\n");
+        if (response != undefined) {
+          // console.log(response[2]?.substring(response[2].indexOf(":") + 1));
+          const businessName = response[0]
+            ?.substring(response[0].indexOf(":") + 1)
+            .replace(/"/g, "");
+          const businessDescription = response[1]?.substring(
+            response[1].indexOf(":") + 1
+          );
+          let businessDomainsString = response[2]?.substring(
+            response[2].indexOf(":") + 1
+          );
+          businessDomainsString.replace(/[\[\]]/g, "");
+          businessDomainsString = businessDomainsString
+            .trim()
+            .slice(1, -1)
+            .replace(/'/g, "");
+          const businessDomains = businessDomainsString.split(",");
 
-      // businessDomainsString = businessDomainsString
-      //   .replace(/[\[\]]/g, "")
-      //   .trim();
-      // const businessDomains = businessDomainsString
-      //   .split(",")
-      //   .map((item) => item.trim());
+          res.status(200).json({
+            businessName: businessName,
+            businessDescription: businessDescription,
+            businessDomains: businessDomains,
+          });
+          return res.end();
+        }
+      }).catch((error) => {
+        console.log(error);
+        return res.status(500);
+      })
 
-      res.status(200).json({
-        businessName: businessName,
-        businessDescription: businessDescription,
-        businessDomains: businessDomains,
-      });
-      return res.end();
-      
-    }
+    // businessDomainsString = businessDomainsString
+    //   .replace(/[\[\]]/g, "")
+    //   .trim();
+    // const businessDomains = businessDomainsString
+    //   .split(",")
+    //   .map((item) => item.trim());
   }
 }
 
