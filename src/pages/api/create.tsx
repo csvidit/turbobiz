8240e9cbf11dd35a6a2afbf46395ec053985d4ba
Vidit Khandelwal <viditk17@gmail.com>
2023-07-16 12:54:46 -0400

    minor change

--- src/pages/api/create.tsx
+++ src/pages/api/create.tsx
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method != 'POST') {
+  if (req.method !== 'POST') {
     return res.status(405).json({error: "METHOD NOT ALLOWED"})
   } else {
     const { category, country, isRemote, businessSize } = req.body.params;
