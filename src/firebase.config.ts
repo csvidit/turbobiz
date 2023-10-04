01ba058e93d104ff47bb56afbdbc9d020f434d78
Vidit Khandelwal <viditk17@gmail.com>
2023-10-04 18:26:13 -0400

    removed client access to all env variables now that everything is done in backend api routes

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -2,28 +2,28 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, initializeFirestore } from "@firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
 
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
 
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  appId: process.env.FIREBASE_APP_ID,
 
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 
   // auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
 
-  // clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  // clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
 // const firebaseAdminConfig = {
 //   credential: firebaseAdmin.credential.cert({
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
 //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 //     privateKey: process.env.FIREBASE_PRIVATE_KEY,
 //   }),
